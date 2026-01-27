ef19cbe90b93963cda2a898e8941e38d3535de51
ParthD <100805460+PartyD1@users.noreply.github.com>
2026-01-27 11:07:44 -0800

    Analyze Guess Function

--- main.py
+++ main.py
@@ -5,3 +5,7 @@ app = FastAPI()
 @app.get("/")
 def root():
     return {"status": "Wordle helper backend running"};
+
+@app.post("/analyze")
+def analyze(guesses: list[str]):
+    return {"remaining_words": 231}
